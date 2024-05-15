11be126eab72488564db384952276c4717ecd9c0
Abhinay Shankar Belde <142843743+08Abhinay@users.noreply.github.com>
2024-05-14 20:10:08 -0400

    syntax check

--- Concepts/Sytax_check.py
+++ Concepts/Sytax_check.py
@@ -1,13 +1,21 @@
-  
+def caesarCipherEncryptor(string, key):
+    # Write your code here.
+    length_of_original = len(string)
+    end_letter_number = ord(string[-1])
+    
+    
+    
+    for number in range(1, key + 1):
+        if end_letter_number == 122:
+            end_letter_number = 96
+        string += chr(end_letter_number + 1)
+        end_letter_number = ord(string[-1])
         
-array = [5, 4,3, 2,1]
+    return string[-length_of_original:]
 
 
-for i in range(len(array)):
-    for j in range(len(array) - 1):
-        if array[j] > array[j + 1]:
-            temp = array[j + 1]
-            array[j + 1] = array[j]
-            array[j] = temp 
-            
-print(array)
+
+# caesarCipherEncryptor("abc", 26)
+
+print(400%26)
+
