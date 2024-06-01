9cf70e2fc59c05adbaf8c550a2f950c58c4e983d
Abhinay Shankar Belde <142843743+08Abhinay@users.noreply.github.com>
2024-06-01 15:19:21 -0400

    Bst Construction

--- Concepts/bstConstruction.py
+++ Concepts/bstConstruction.py
@@ -5,15 +5,6 @@ class Node(object):
         self.right = None 
         
 if __name__ == '__main__':
-    # root = Node('F')
-    # root.left = Node('B')
-    # root.right = Node('G')
-    # root.left.left = Node('A')
-    # root.left.right = Node('D')
-    # root.right.right = Node('I')
-    # root.left.right.left=Node('C')
-    # root.left.right.right = Node('E')
-    # root.right.right.left = Node('H')
     root = Node(10)
     root.left = Node(5)
     root.left.left = Node(2)
@@ -78,46 +69,3 @@ def remove(root, value, parentNode = None):
     pass
 
        
-
-def pre_order_traversal(root):
-    
-    if root is None:
-        return
-    
-    print(root.value, end=' ')
-    pre_order_traversal(root.left)    
-    pre_order_traversal(root.right)
-
-# def in_order_traversal(root):
-     
-#     if root:
-
-#         in_order_traversal(root.left)
-#         print(root.value, end=' ')
-#         in_order_traversal(root.right)
- 
-# def post_order_traversal(root):
-   
-#     if root:
-
-#         in_order_traversal(root.left)
-#         in_order_traversal(root.right)
-#         print(root.value, end=' ')
-#     pass
-
-
-
-# Function call
-# print("Preorder traversal of binary tree is:")
-pre_order_traversal(root) 
-# print()
-# in_order_traversal(root)
-# print()
-# post_order_traversal(root)
-
-
-
-
-    
-
-
